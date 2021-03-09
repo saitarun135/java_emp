c51ab13c0e50bbb8949990b626e259c2c97c6757
saitarun135 <saitarun135@gm.com>
2021-03-09 17:39:01 +0530

    present or not

--- Empwage.java
+++ Empwage.java
@@ -1,1 +1,10 @@
-#welcome to the employee problems
+public class Empwage{ 
+	public static void main(String args[]){
+	int IS_FULLTIME=1;
+		double empchk=Math.floor(Math.random() *10 %2);
+		if(empchk == IS_FULLTIME)
+			System.out.println("Employee is present");
+		else
+		System.out.println("Employee is absent");
+}
+}
